204306a8d716ee38af99fa3180fbfe6e4a2e160d
Art8mish <111497619+Art8mish@users.noreply.github.com>
2022-09-22 14:48:42 +0300

    Update libraries.h

--- include/libraries.h
+++ include/libraries.h
@@ -8,14 +8,9 @@
 #include "sorting_func.h"
 #include "io.h"
 
-<<<<<<< HEAD
-=======
-
 #ifndef INPUT_FILE_NAME
 #define INPUT_FILE_NAME "onegin.txt"
 #endif
-
->>>>>>> 4c55c965d09fff2a65115dbaf16df65d55a4dc16
 
 //#define SOFT_ASSERT
 
@@ -40,11 +35,7 @@
         {                                   \
             if (!(condition))               \
             {                               \
-<<<<<<< HEAD
-              /*error_code = error_type;*/  \
-=======
                 /*error_code = error_type;*/\
->>>>>>> 4c55c965d09fff2a65115dbaf16df65d55a4dc16
                 return error_type;          \
             }                               \
         } while(false)
